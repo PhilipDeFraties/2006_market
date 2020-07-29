506d26492fff1a712cb9b6650e1fc1544045a3b1
Philip DeFraties <philipjfry338@gmail.coom>
2020-07-29 09:53:14 -0600

    Add market class with add_vendor method.

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -33,7 +33,7 @@ class VendorTest < MiniTest::Test
     vendor = Vendor.new("Rocky Mountain Fresh")
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    
+
     vendor.stock(item1, 30)
 
     assert_equal ({item1 => 30}), vendor.inventory
@@ -44,6 +44,7 @@ class VendorTest < MiniTest::Test
     assert_equal 55, vendor.check_stock(item1)
 
     vendor.stock(item2, 12)
+    assert_equal ({item1 => 55, item2 => 12}), vendor.inventory
   end
 
 
